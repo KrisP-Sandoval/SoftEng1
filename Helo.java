b3360507c4bcf7c62c08fdf25883edd29ac393d2
KrisP-Sandoval <142380532+KrisP-Sandoval@users.noreply.github.com>
2023-08-16 14:27:03 +0800

    Create Helo.java

---
+++ Helo.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, Sofware Engineering 1!");
+    System.out.println("Student Name: Sandoval, Kris Patrick Luisse F.");
+  }
+}
