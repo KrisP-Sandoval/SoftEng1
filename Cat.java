a2e054307fbadf1ed0286e70ade8c48bf07e7e4b
KrisP-Sandoval <142380532+KrisP-Sandoval@users.noreply.github.com>
2023-10-02 22:12:01 +0800

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,27 @@
+package factoryPattern;
+
+public class Cat implements Pet {
+    private Integer noOfLives;
+
+    public Integer getNoOfLives() {
+        return noOfLives;
+    }
+
+    public void setNoOfLives(Integer noOfLives) {
+        this.noOfLives = noOfLives;
+    }
+
+    public void displayNoOfLives() {
+        System.out.println("Number of lives: " + noOfLives);
+    }
+
+    @Override
+    public String makeSound() {
+        return "Meow, meow!";
+    }
+
+    @Override
+    public String play() {
+        return "Catching/pursuing moving objects!";
+    }
+}
