188fa77c2f5f2a638a758e52648bdf15a1cca3ce
KrisP-Sandoval <142380532+KrisP-Sandoval@users.noreply.github.com>
2023-10-02 22:37:04 +0800

    Create Dog.java

---
+++ factoryPattern/Dog.java
@@ -0,0 +1,27 @@
+package factoryPattern;
+
+public class Dog implements Pet {
+    private String breed;
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void displayBreed() {
+        System.out.println("Dog breed: " + breed);
+    }
+  
+    @Override
+    public String makeSound() {
+        return "Aw, aw!";
+    }
+
+    @Override
+    public String play() {
+        return "Catching objects that you throw!";
+    }
+}
